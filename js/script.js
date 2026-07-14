f5a8b8c7b6b1518bc0bf0147075e6e5b992c6956
elok122130014-cpu <elok.122130014@student.itera.ac.id>
2026-07-14 16:00:03 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+if(menuToggle){
+
+    menuToggle.addEventListener("click", () => {
+
+        menu.classList.toggle("active");
+
+    });
+
+}
